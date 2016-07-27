d43c061eaa4665dd23e56ef2c06d047c0440209c
Katy Atherholt <katarina.atherholt@gmail.com>
2016-07-27 17:15:14 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-script.js
+// set "bank" 
+// bank = 100 to start, bets between 5-10 and chooses a number between 1 and 10
+// user sets bet and chooses number
+// win doubles bet, loss subtracts bet amount
+// verify bank has enough money, enter bet and choice again, on enter, run comparison again, otherwise, game over
